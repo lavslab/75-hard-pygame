417b56f26e0c9534698d79b9144798100964d1e6
TheresaCBennett <140468048+TheresaCBennett@users.noreply.github.com>
2026-09-09 18:26:46 -0400

    Replace placeholder with player sprite

--- main.py
+++ main.py
@@ -9,23 +9,29 @@ HEIGHT = 500
 
 # colors
 PINK = (255, 220, 235)
-HOT_PINK = (255, 105, 180)
 DARK_PINK = (210, 80, 140)
 
 # create game window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("75 Hard: Lav Edition")
 
+# load player image
+lav_image = pygame.image.load("assets/lav_idle.png").convert_alpha()
+
+# resize player image
+lav_image = pygame.transform.scale(lav_image, (70, 100))
+
 # add time clock
 clock = pygame.time.Clock()
 
 # player settings
 lav_x = 100
-lav_y = 380
-lav_width = 50
-lav_height = 70
+lav_y = 350
+lav_width = 70
+lav_height = 100
 lav_speed = 5
 
+# jumping settings
 lav_y_velocity = 0
 gravity = 1
 jump_strength = -15
@@ -48,33 +54,35 @@ while running:
         lav_x += lav_speed
 
     # jump
-    if keys[pygame.K_SPACE] and lav_y == 380:
+    if keys[pygame.K_SPACE] and lav_y == 350:
         lav_y_velocity = jump_strength
 
+    # gravity
     lav_y_velocity += gravity
     lav_y += lav_y_velocity
 
     # keep Lav on the ground
-    if lav_y >= 380:
-        lav_y = 380
+    if lav_y >= 350:
+        lav_y = 350
         lav_y_velocity = 0
 
     # pink background screen
     screen.fill(PINK)
 
     # ground
-    pygame.draw.rect(screen, DARK_PINK, (0, 450, WIDTH, 50))
-
-    # draw player
     pygame.draw.rect(
         screen,
-        HOT_PINK,
-        (lav_x, lav_y, lav_width, lav_height)
+        DARK_PINK,
+        (0, 450, WIDTH, 50)
     )
+
+    # draw player
+    screen.blit(lav_image, (lav_x, lav_y))
 
     # update after each drawing aka finished frame
     pygame.display.update()
 
+    # keep game running at 60 FPS
     clock.tick(60)
 
 pygame.quit()
